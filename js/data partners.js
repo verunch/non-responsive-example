75cff4ce617566d970b1b443de01dad55a17dca8
verunch <vera.bakerova@mail.ru>
2016-06-29 23:11:48 +0300

    my first commit
    add files

---
+++ js/data partners.js
@@ -0,0 +1,11 @@
+var partners = 
+        [
+        
+          {img:"img/1.png"},
+          {img:"img/2.png"},
+          {img:"img/3.png"},
+          {img:"img/4.png"},
+          {img:"img/5.png"}
+      ];
+     
+$('#PartnersImgRow').tmpl(partners).appendTo('#partnersImg');
---
+++ js/myjs.js
@@ -0,0 +1,20 @@
+$(function() {
+        $('.single-item').slick({
+        	 nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+        prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>'
+        });
+
+
+        $('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+        prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>'
+});
+	
+ 
+ 
+
+
+});
